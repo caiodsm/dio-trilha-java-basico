7e59808478ab24e35a538b287bc683a2dcacee75
Caio Miranda <caiomirand@gmail.com>
2024-05-03 12:42:38 -0300

    Realizado o Exercicio da conta do banco

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //Exibir as mensages para o nosso usuário
+        System.out.println("Por favor, digite o seu número !");
+        int contaUsuario = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        String agenciausuario = scanner.next();
+
+        String scanner2 = scanner.nextLine();
+        System.out.println("Por favor, digite o seu Nome !");
+        String nomeCliente = scanner.nextLine();
+       
+        System.out.println("Por favor, digite o seu saldo !");
+        double saldoCliente = scanner.nextDouble();
+        //Obter pela scanner os valores digitados no terminal
+        
+        
+
+        //Exibir  a mensagem final usando concat
+        String mensagemFinal = "Olá ";
+        
+        mensagemFinal = mensagemFinal.concat(nomeCliente +  ", obrigado por criar uma conta em nosso banco, sua agência é ").concat(agenciausuario + ", conta ").concat(contaUsuario + " e seu saldo ").concat(saldoCliente + " já está disponível para saque");
+        System.out.println(mensagemFinal);
+        
     }
 }
